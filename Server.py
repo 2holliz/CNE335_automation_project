c6533e53612956c9aa966a022ae5ae9b0899384d
Mustafa22 <maalghuraibawi@student.rtc.edu>
2025-03-18 19:52:36 -0700

    Revised main.py & Server.py
    Completed all 3 TODOs, Pinged public IPv4 successfully.

--- Server.py
+++ Server.py
@@ -1,3 +1,5 @@
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
@@ -7,4 +9,5 @@ class Server:
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        response = os.system("ping -n 4 " + self.server_ip)
+        return self.server_ip
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# Mustafa Alghuraibawi
+# CNE 335 Winter
+
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Mustafa")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    EC2server = Server('34.211.5.59')
     # TODO - Call Ping method and print the results
+    print(EC2server.ping())
